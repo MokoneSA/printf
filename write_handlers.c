0048dc285085839e91e379ee9df9cf997b0a760d
MokoneSA <simonlephotojr@gmail.com>
2022-10-21 13:04:21 +0000

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- write_handlers.c
+++ write_handlers.c
@@ -232,8 +232,8 @@ int write_pointer(char buffer[], int ind, int length,
 		{
 			if (extra_c)
 				buffer[--padd_start] = extra_c;
-				buffer[1] = '0';
-				buffer[2] = 'x';
+			buffer[1] = '0';
+			buffer[2] = 'x';
 			return (write(1, &buffer[padd_start], i - padd_start) +
 				write(1, &buffer[ind], length - (1 - padd_start) - 2));
 		}
